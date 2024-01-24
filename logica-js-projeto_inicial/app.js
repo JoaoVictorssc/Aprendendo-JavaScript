6cc2d65ae034a4849688ae9a24e7b2faff1394d9
Joao Victor <jvrp9621@gmail.com>
2024-01-24 19:54:42 -0300

    criação do repositorio de JS

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Boas-vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número de 1 a 10');
+
+if (chute == numeroSecreto) {
+    console.log('isso aí! Você descobriu o número secreto (5)');
+}
