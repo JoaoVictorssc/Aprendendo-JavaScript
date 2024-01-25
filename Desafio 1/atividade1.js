6ec0d7abfd416b7c997f795c4d3504fce3a041c3
Joao Victor <jvrp9621@gmail.com>
2024-01-25 15:12:50 -0300

    conclusao

--- Desafio 1/atividade1.js
+++ Desafio 1/atividade1.js
@@ -1,12 +1,14 @@
 alert('Boas vindas ao nosso site!');
 let nome = "lua";
 let idade = 25;
+console.log(idade)
 let numeroDeVendas = 50;
+console.log(numeroDeVendas)
 let saldoDisponivel = 1000;
 let mensagemDeErro = 'Erro! Preencha todos os campos';
 alert(mensagemDeErro);
 let  = prompt('Digite o seu nome');
-let  = prompt ('Digite sua idade')
+let  = prompt ('Digite sua idade');
 if (idade >= 18) {
     alert('Pode tirar carteira')
 }
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,21 @@
 alert('Boas-vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
-let chute = prompt ('Escolha um número de 1 a 10');
-
-if (chute == numeroSecreto) {
-    console.log('isso aí! Você descobriu o número secreto (5)');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() *numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 a ${numeroMaximo}`);
+    if (chute == numeroSecreto) {
+        break
+    } else{ 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else{
+            alert(`O número secreto é maior que ${chute}`);
+        }   
+        tentativas++
+    }
 }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+alert(`isso aí! Você descobriu o número secreto ${numeroSecreto} com um total de  ${tentativas} ${palavraTentativa}.`);
