2cd95100c906bdeddd4d12ef43885bf5ac8d0a63
Joao Victor <jvrp9621@gmail.com>
2024-01-25 11:28:25 -0300

    atualizações

--- Desafio 1/atividade1.js
+++ Desafio 1/atividade1.js
@@ -3,12 +3,10 @@ let nome = "lua";
 let idade = 25;
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
-alert ("Erro! Preencha todos os campos");
 let mensagemDeErro = 'Erro! Preencha todos os campos';
 alert(mensagemDeErro);
-let nome = prompt('Digite o seu nome');
-let idade = prompt ('Digite sua idade')
-let idade = prompt ('Digite sua idade');
+let  = prompt('Digite o seu nome');
+let  = prompt ('Digite sua idade')
 if (idade >= 18) {
     alert('Pode tirar carteira')
 }
